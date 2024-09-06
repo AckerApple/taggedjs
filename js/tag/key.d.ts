ba076ccc14595280e443408cb9bc97b35790ca72
Acker Apple <y>
2024-09-06 17:11:01 -0400

    dist patch

--- js/tag/key.d.ts
+++ js/tag/key.d.ts
@@ -1,5 +1,5 @@
-import { DomTag, StringTag } from "./index";
-import { ToTag } from "./tag.types";
+import { DomTag, StringTag } from "./index.js";
+import { ToTag } from "./tag.types.js";
 /** Used to give unique value to an array of tag content. Should not be an object */
 export declare function key(arrayValue: string | number | null): {
     html: StringTag | DomTag | ToTag;
