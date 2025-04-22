7b6ba46c2842e9cd6d83169d880db0ffdf0240f4
Acker Apple <y>
2025-04-22 15:00:42 -0400

    dist patch

--- js/tag/html.d.ts
+++ js/tag/html.d.ts
@@ -2,11 +2,12 @@ import { InputElementTargetEvent } from '../interpolations/attributes/ElementTar
 import { LikeObjectChildren } from '../interpolations/optimizers/LikeObjectElement.type.js';
 import { RegularValue } from './update/processRegularValue.function.js';
 import { Tag } from './Tag.type.js';
-type InputCallback = ((e: InputElementTargetEvent) => any);
-export type TagValues = (InputCallback | RegularValue | null | unknown | undefined | object)[];
+export type InputCallback = ((e: InputElementTargetEvent) => unknown);
+/** represents a single value within html`<div>${value}</div>`. The data typing of "& unknown" is to allow anything AND STILL infer functions have one argument if "e"  */
+export type TagValue = (InputCallback | RegularValue | object) & unknown;
+export type TagValues = TagValue[];
 /** Used as html`<div></div>` */
 export declare function html(strings: string[] | TemplateStringsArray, ...values: TagValues): Tag;
 export declare namespace html {
     var dom: (dom: LikeObjectChildren, ...values: TagValues) => import("./DomTag.type.js").DomTag;
 }
-export {};
