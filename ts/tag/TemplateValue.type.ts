41f800d386296590a26bc425df4d5572c88d46dc
Acker Apple <y>
2025-05-22 17:18:06 -0400

    patch

--- ts/tag/TemplateValue.type.ts
+++ ts/tag/TemplateValue.type.ts
@@ -1,9 +1,9 @@
 import { TemplaterResult } from './getTemplaterResult.function.js'
 import { RegularValue } from './update/processRegularValue.function.js'
 import { Callback } from '../interpolations/attributes/bindSubjectCallback.function.js'
-import { StringTag } from './StringTag.type.js'
 import { Subject } from '../subject/index.js'
 import { SubscribeValue } from '../tagJsVars/subscribe.function.js'
+import { Tag } from './Tag.type.js'
 
 // what can be put down with ${}
-export type TemplateValue = StringTag | SubscribeValue | TemplaterResult | (StringTag | TemplaterResult)[] | RegularValue | Subject<unknown> | Callback
+export type TemplateValue = Tag | SubscribeValue | TemplaterResult | (Tag | TemplaterResult)[] | RegularValue | Subject<unknown> | Callback
