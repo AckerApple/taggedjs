8753d2ce859bbcaca26272f7359eb5013d9414ca
Acker Apple <y>
2025-05-22 17:18:00 -0400

    dist patch

--- js/tag/TemplateValue.type.d.ts
+++ js/tag/TemplateValue.type.d.ts
@@ -1,7 +1,7 @@
 import { TemplaterResult } from './getTemplaterResult.function.js';
 import { RegularValue } from './update/processRegularValue.function.js';
 import { Callback } from '../interpolations/attributes/bindSubjectCallback.function.js';
-import { StringTag } from './StringTag.type.js';
 import { Subject } from '../subject/index.js';
 import { SubscribeValue } from '../tagJsVars/subscribe.function.js';
-export type TemplateValue = StringTag | SubscribeValue | TemplaterResult | (StringTag | TemplaterResult)[] | RegularValue | Subject<unknown> | Callback;
+import { Tag } from './Tag.type.js';
+export type TemplateValue = Tag | SubscribeValue | TemplaterResult | (Tag | TemplaterResult)[] | RegularValue | Subject<unknown> | Callback;
